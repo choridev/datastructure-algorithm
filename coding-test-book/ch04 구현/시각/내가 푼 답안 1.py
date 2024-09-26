8268f41138f1a52d7870dc91f7fdd3f2c2a8eed2
Chori <yidodeveloper@gmail.com>
2024-09-26 14:37:41 +0000

    시각 문제 해결

---
+++ coding-test-book/ch04 구현/시각/내가 푼 답안 1.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+
+result = 0
+for h in range(n + 1):
+    for m in range(60):
+        for s in range(60):
+            if '3' in str(h) or '3' in str(m) or '3' in str(s):
+                result += 1
+
+print(result)
