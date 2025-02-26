807b051d6f712e70aef9af38e11e2825a2d8ff9b
yidodeveloper <yidodeveloper@gmail.com>
2025-02-26 21:41:24 +0900

    🎨 Clean code

--- baekjoon/greedy/2839/source_code.py
+++ baekjoon/greedy/2839/source_code.py
@@ -3,23 +3,19 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-optimal_five = 0
-optimal_three = 0
+result = int(1e9)
 
-max_five = n // 5 or 1
-remainder = 0
-for i in range(max_five, 0, -1):
-    if (n - (5 * i)) % 3 == 0:
-        optimal_five = i
-        remainder = n - (5 * optimal_five)
-        optimal_three = remainder // 3
-        break
-    else:
-        if n % 3 == 0:
-            optimal_three = n // 3
+quotient_of_five = n // 5
 
-result = optimal_five + optimal_three
-if result == 0:
-    print(-1)
-else:
-    print(result)
+for count_of_five in range(quotient_of_five + 1):
+    count = 0
+    remainder_of_five = (n - (count_of_five * 5))
+    remainder_of_three = remainder_of_five % 3
+    if remainder_of_three == 0:
+       quotient_of_three = remainder_of_five // 3 
+       count = count_of_five + quotient_of_three
+       if count < result:
+           result = count
+
+result = -1 if result == int(1e9) else result
+print(result)
